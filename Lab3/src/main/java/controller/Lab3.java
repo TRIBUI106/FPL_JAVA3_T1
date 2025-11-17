53503578ceb6c1a0cad3ef5545de2ccac15c3b7f
chez1s <71917288+TRIBUI106@users.noreply.github.com>
2025-11-17 15:39:20 +0700

    Base(Lab4)

--- Lab3/src/main/java/controller/Lab3.java
+++ Lab3/src/main/java/controller/Lab3.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
