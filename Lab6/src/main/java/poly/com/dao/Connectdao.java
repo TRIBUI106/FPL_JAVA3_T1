14b87c1cc7a7847cd251df30e180db7242ce081c
chez1s <toptrainghieml@gmail.com>
2025-11-28 01:35:02 +0700

    Update Connectdao.java

--- Lab6/src/main/java/poly/com/dao/Connectdao.java
+++ Lab6/src/main/java/poly/com/dao/Connectdao.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class Connectdao
 {
 	protected static Connection conn;
-	private static final String URL = "jdbc:sqlserver://chez1s.duckdns.org:1433;databaseName=ABCNews;encrypt=false;trustServerCertificate=true;";
-	private static final String USER = "sa";
-	private static final String PASS = "123"; // pass cậu đặt ở docker
+	private static final String URL = "jdbc:mariadb://yeume-enterprise.edu.vn:3306;databaseName=yeumeent_dihoc";
+	private static final String USER = "yeumeent_dihoc";
+	private static final String PASS = "Caigivaytroi@123"; // pass cậu đặt ở docker
 
 	public static Connection getConnection() throws Exception {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
