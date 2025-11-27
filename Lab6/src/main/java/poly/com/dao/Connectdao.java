2e930baea466de9ee34682bc3a0d1830bf2d3f23
chez1s <toptrainghieml@gmail.com>
2025-11-28 01:25:31 +0700

    Update Connectdao.java

--- Lab6/src/main/java/poly/com/dao/Connectdao.java
+++ Lab6/src/main/java/poly/com/dao/Connectdao.java
@@ -5,13 +5,20 @@ import java.sql.DriverManager;
 
 public class Connectdao
 {
-	 protected static Connection conn;
-	   
+	protected static Connection conn;
+	private static final String URL = "jdbc:sqlserver://chez1s.duckdns.org:1433;databaseName=ABCNews;encrypt=false;trustServerCertificate=true;";
+	private static final String USER = "sa";
+	private static final String PASS = "123"; // pass cậu đặt ở docker
+
+	public static Connection getConnection() throws Exception {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		return DriverManager.getConnection(URL, USER, PASS);
+	}
+
 	 public Connectdao() {
 	    try {
-	        String url = "jdbc:mysql://localhost:3306/lab6_java3_csdl?useUnicode=true&characterEncoding=UTF-8";
 	        Class.forName("com.mysql.cj.jdbc.Driver");
-	        conn = DriverManager.getConnection(url, "root", "");
+	        conn = DriverManager.getConnection(URL, USER, PASS);
 	        System.out.println("Kết nối MySQL thành công!");
 	    	} 
 	    catch (Exception ex)
