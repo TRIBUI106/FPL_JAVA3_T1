4c92a5778979d1c7a7096324de4ba06e51a10392
chez1s <toptrainghieml@gmail.com>
2025-11-28 22:57:42 +0700

    Fix(Redirect): /admin -> /admin/dashboard

--- Assignment/src/main/java/controller/admin/DashboardController.java
+++ Assignment/src/main/java/controller/admin/DashboardController.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet("/admin/dashboard")
+@WebServlet({"/admin/dashboard", "/admin"})
 public class DashboardController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -15,6 +15,11 @@ public class DashboardController extends HttpServlet {
             response.sendRedirect(request.getContextPath() + "/login");
             return;
         }
+        
+        if ( (request.getContextPath() + "/" + request.getRequestURL()).equals("/admin") ) {
+        	response.sendRedirect("/admin/dashboard");
+        }
+        
         request.getRequestDispatcher("/admin/dashboard.jsp").forward(request, response);
     }
 }
