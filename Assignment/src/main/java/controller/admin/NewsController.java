750e9ca258f63fd10960641af853a356f5a7760c
chez1s <toptrainghieml@gmail.com>
2025-12-12 13:07:09 +0700

    Update NewsController.java

--- Assignment/src/main/java/controller/admin/NewsController.java
+++ Assignment/src/main/java/controller/admin/NewsController.java
@@ -67,9 +67,6 @@ public class NewsController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html; charset=UTF-8");
 
         String action = req.getParameter("action");
         String id = req.getParameter("id");
